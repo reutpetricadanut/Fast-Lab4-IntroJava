f7001197b50f3dbb8762043843b39ce2825c19f4
dan <petrica_danut@yahoo.com>
2017-03-29 12:32:15 +0200

    problema nr.2

---
+++ src/number3.java
@@ -0,0 +1,16 @@
+/**
+ * Created by dan on 3/29/2017.
+ */
+public class number3 {
+    public static void main(String[] args) {
+        int n=20;
+        int sum= 0;
+        for (n=0; n<=20;n++){
+            sum = sum + n;
+        }
+            System.out.println(sum);
+        }
+
+
+    }
+
